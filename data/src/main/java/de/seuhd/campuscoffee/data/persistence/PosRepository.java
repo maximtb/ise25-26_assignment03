ba380648c02dfbb20107df9d02ee13f38a6d39a6
Sebastian Baltes <research@sbaltes.com>
2025-11-26 10:12:27 +0100

    Remove unused import

--- data/src/main/java/de/seuhd/campuscoffee/data/persistence/PosRepository.java
+++ data/src/main/java/de/seuhd/campuscoffee/data/persistence/PosRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
-
 /**
  * Repository for persisting point-of-sale (POS) entities.
  */
